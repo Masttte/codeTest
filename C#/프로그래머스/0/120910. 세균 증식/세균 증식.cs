74e343f99afe142605504b11740cdb4cc2fdc919
Masttte <peonyak08@gmail.com>
2025-11-28 19:29:29 +0900

    Update 세균 증식.cs

--- C#/프로그래머스/0/120910. 세균 증식/세균 증식.cs
+++ C#/프로그래머스/0/120910. 세균 증식/세균 증식.cs
@@ -3,7 +3,7 @@ using System;
 public class Solution {
     public int solution(int n, int t)
 {
-    var step = Math.Pow(2, t);
+    double step = Math.Pow(2, t); //거듭 제곱 (2^t)
     int answer = n * (int)step;
     return answer;
 }
