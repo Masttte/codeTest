1463095676fc0d9feda012af1ece7a8a3bc2c2ec
Masttte <peonyak08@gmail.com>
2025-11-30 22:28:38 +0900

    Update n 번째 원소까지.cs

--- C#/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cs
+++ C#/프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.cs
@@ -10,3 +10,13 @@ public class Solution {
         return answer;
     }
 }
+
+
+//다른 답
+public class Solution {
+    public int[] solution(int[] num_list, int n) {
+        int[] answer = new int[] {};
+        answer = num_list.Take(n).ToArray();
+        return answer;
+    }
+}
