ed39e19587645e047ea2191249c783ee415211e0
Masttte <peonyak08@gmail.com>
2025-12-01 18:52:48 +0900

    Update 더 크게 합치기.cs

--- C#/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
+++ C#/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
@@ -3,8 +3,8 @@ using System;
 public class Solution {
     public int solution(int a, int b) 
     {
-        int aNum = int.Parse($"{a}{b}"); // 스트링 지정없이 이렇게 하면 됨
-        int bNum = int.Parse($"{b}{a}");
-        return Math.Max(aNum, bNum); //Math.Max 는 더 큰값 반환, 같으면 앞의거 반환
+        int ab = int.Parse($"{a}{b}"); // 스트링 지정없이 이렇게 하면 됨
+        int ba = int.Parse($"{b}{a}");
+        return Math.Max(ab, ba); //Math.Max 는 더 큰값 반환, 같으면 앞에꺼 반환
     }
 }
