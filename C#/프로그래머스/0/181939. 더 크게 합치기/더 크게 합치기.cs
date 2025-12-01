48ab2591dd8e24a627275d2363471e776e42c067
Masttte <peonyak08@gmail.com>
2025-12-01 18:49:12 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.03 ms, Memory: 31.6 MB -BaekjoonHub

--- C#/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
+++ C#/프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
@@ -1,16 +1,10 @@
 using System;
 
 public class Solution {
-    public int solution(int a, int b) {
-        string ab = $"{a}{b}";
-        string ba = $"{b}{a}";
-        int abP = int.Parse(ab);
-        int baP = int.Parse(ba);
-
-        if (abP >= baP)
-        {
-           return abP;
-        }
-        else { return baP; }
+    public int solution(int a, int b) 
+    {
+        int aNum = int.Parse($"{a}{b}"); // 스트링 지정없이 이렇게 하면 됨
+        int bNum = int.Parse($"{b}{a}");
+        return Math.Max(aNum, bNum); //Math.Max 는 더 큰값 반환, 같으면 앞의거 반환
     }
 }
