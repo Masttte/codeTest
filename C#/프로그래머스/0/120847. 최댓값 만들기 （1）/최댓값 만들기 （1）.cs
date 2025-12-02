d3c6317d943fcc9bb75dd0d25f53757cc55e4672
Masttte <peonyak08@gmail.com>
2025-12-02 21:10:32 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 2.38 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ C#/프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers) {
+        Array.Sort(numbers);
+        int n = numbers.Length;
+
+        return numbers[n - 1] * numbers[n - 2];;
+    }
+}
