e172c360052148de51e21a3bf0f0c7dea4228a12
Masttte <peonyak08@gmail.com>
2025-12-12 12:20:13 +0900

    Update 카드 뭉치.cs

--- C#/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
+++ C#/프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
@@ -9,14 +9,14 @@ public class Solution {
         for (int i = 0; i < goal.Length; i++)
         {
             // out if index 방지를 위해 배열 길이가 0이면 무시 (이걸 먼저 체크)
-            if (!(cards1.Length == 0) && goal[i] == cards1[0])
+            if (cards1.Length > 0 && goal[i] == cards1[0])
             { //cards1[0] 의 데이터를 지우고 앞으로 당긴다.
                 string[] temp = new string[cards1.Length - 1];
                 Array.Copy(cards1, 1, temp, 0, cards1.Length - 1); // cards1의 1번째 인덱스부터 temp에 복사
                 cards1 = temp;
                 
             }
-            else if (!(cards2.Length == 0) && goal[i] == cards2[0])
+            else if (cards2.Length > 0 && goal[i] == cards2[0])
             { //cards2[0] 의 데이터를 지우고 앞으로 당긴다.
                 string[] temp = new string[cards2.Length - 1];
                 Array.Copy(cards2, 1, temp, 0, cards2.Length - 1);
