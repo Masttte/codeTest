c2591b426ba957fdf3136455fbcaac819d811355
EonyakPark <peonyak08@gmail.com>
2026-01-06 16:15:58 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.32 ms, Memory: 31.7 MB -BaekjoonHub

--- C#/프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cs
+++ C#/프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.cs
@@ -4,13 +4,12 @@ public class Solution
 {
     public int[] solution(int num, int total)
     {
-        // num의 순서 배열을 담을 배열
-        int[] sortNum = new int[num];
+        int[] answer = new int[num];
 
         // total을 num으로 나눈 몫을 구한다
         int quotient = total / num;
 
-        // num 이 짝수인경우와 홀수인 경우로 나눔
+        // num 이 홀수인 경우
         if (num % 2 == 1)
         {
             // 홀수인 경우 num을 2로 나눈 몫이 배열의 중앙인덱스
@@ -20,22 +19,22 @@ public class Solution
             // 시작 숫자를 1씩 증가하며 정렬
             for (int i = 0; i < num; i++)
             {
-                sortNum[i] = startNumber + i;
+                answer[i] = startNumber + i;
             }
         }
-        else
+        else // num 이 짝수인 경우
         {
-            // 짝수인 경우도 중앙인덱스 몫은 동일
+            // 짝수인 경우 중앙 오른쪽을 중앙인덱스로 함
             int centerIndex = num / 2;
             // 중앙 인덱스에서 중앙 인덱스 +1 만큼을 빼면 시작 숫자
             int startNumber = quotient - centerIndex + 1;
             // 시작 숫자를 1씩 증가하며 정렬
             for (int i = 0; i < num; i++)
             {
-                sortNum[i] = startNumber + i;
+                answer[i] = startNumber + i;
             }
         }
 
-        return sortNum;
+        return answer;
     }
 }
