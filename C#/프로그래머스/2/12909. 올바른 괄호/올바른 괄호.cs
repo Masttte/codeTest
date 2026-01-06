4e475cb009764c7d34a18df9636054c814d23ff6
EonyakPark <peonyak08@gmail.com>
2026-01-06 17:51:29 +0900

    [level 2] Title: 올바른 괄호, Time: 0.68 ms, Memory: 31.2 MB -BaekjoonHub

--- C#/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
+++ C#/프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cs
@@ -4,34 +4,23 @@ public class Solution
 {
     public bool solution(string s)
     {
-        // 첫 스트링이 ) 인경우
-        // 끝 스트링이 ( 인 경우
-        if (s[0] == ')' || s[s.Length - 1] == '(')
+        int count = 0;
+
+        foreach (char c in s)
         {
-            return false;
+            if (c == '(')
+            {
+                count++;
+            }
+            else
+            {
+                if (count == 0)
+                    return false;
+
+                count--;
+            }
         }
 
-        // '('의 개수와 ')'의 개수가 동일하지 않은경우
-        if (s.Split('(').Length != s.Split(')').Length)
-        {
-            return false;
-        }
-
-        // s 를 순회하면서 '('의 개수보다 ')'가 많아지는 순간
-        int open = 0;
-        int close = 0;
-        foreach (char symbol in s)
-        {
-            if (symbol == '(')
-                open++;
-
-            if (symbol == ')')
-                close++;
-
-            if (close > open)
-                return false;
-        }
-
-        return true;
+        return count == 0;
     }
 }
